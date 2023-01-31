f6e502e35c7cc0c22032ab589d46941b90d93313
iegad <iegad@vip.qq.com>
2023-01-31 18:36:06 +0800

    0.0.4.230131b
    添加linux perf监控脚本

---
+++ xq/start_perf.sh
@@ -0,0 +1,11 @@
+# !/bin/sh
+
+pid=`pidof $1`
+if [ -z "$pid" ]; then
+	echo "[$1] is not running...\n";
+	exit;
+fi
+
+mkdir perf
+`sudo perf record -e cpu-clock -g -p $pid -o ./perf/$1.data`
+echo "press [Ctl+C] to stop collect samples";
